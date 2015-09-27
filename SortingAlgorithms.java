552293acc81389ac91c6614b1607b2136fe01a1f
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2015-09-27 18:56:10 +0800

    Add functionality for BubbleSort.

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,3 +1,43 @@
-public class SortingAlgorithm {
+public class SortingAlgorithms {
+	public final int ARRAY_SIZE = 5;
+	public final String BUBBLE = "bubble";
+	public final String INSERT = "insert";
+	public final String SELECT = "select";
+	public int[] arrayA = {5, 4, 3, 2, 1};
+	public int[] arrayB = {6, 10, 7, 9, 8};
+	public int[] arrayC = {11, 12, 13, 14, 15};
+	
+	public SortingAlgorithms() {
+	}
+	
+	public void sort(String type) {
+		switch (type) {
+			case BUBBLE: bubbleSort(arrayA); bubbleSort(arrayB); bubbleSort(arrayC); break;
+			default: break;
+		}
+	}
+	
+	public void bubbleSort(int[] inputArray) {
+		
+	}
+	
+	public void print() {
+		printArray(arrayA);
+		printArray(arrayB);
+		printArray(arrayC);
+		System.out.print("|\n");
+	}
+	
+	public void printArray(int[] inputArray) {
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			System.out.print("|");
+			System.out.print(inputArray[i]);
+		}
+	}
 
+	public static void main(String[] args){
+		SortingAlgorithms bubbleArray = new SortingAlgorithms();
+		
+		bubbleArray.print();
+	}
 }
