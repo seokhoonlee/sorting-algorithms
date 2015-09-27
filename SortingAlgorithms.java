f9f97c259007533531a7e620e7d5eb3cc18a0d8b
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2015-09-27 19:14:14 +0800

    Add InsertSort

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -3,9 +3,9 @@ public class SortingAlgorithms {
 	public final String BUBBLE = "bubble";
 	public final String INSERT = "insert";
 	public final String SELECT = "select";
-	public int[] arrayA = {5, 4, 3, 2, 1};
-	public int[] arrayB = {6, 10, 7, 9, 8};
-	public int[] arrayC = {11, 12, 13, 14, 15};
+	public int[] arrayA = {5, 4, 3, 2, 1}; // descending order
+	public int[] arrayB = {6, 10, 7, 9, 8}; // random order
+	public int[] arrayC = {11, 12, 13, 14, 15}; // ascending order
 	
 	public SortingAlgorithms() {
 	}
@@ -13,13 +13,44 @@ public class SortingAlgorithms {
 	public void sort(String type) {
 		switch (type) {
 			case BUBBLE: bubbleSort(arrayA); bubbleSort(arrayB); bubbleSort(arrayC); break;
+			case INSERT: insertSort(arrayA); insertSort(arrayB); insertSort(arrayC); break;
 			default: break;
 		}
 	}
 	
 	public void bubbleSort(int[] inputArray) {
+		int temp;
 		
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			for (int j = 0; j < ARRAY_SIZE - i - 1; j++) {
+				if (inputArray[j] > inputArray[j + 1]) {
+					temp = inputArray[j];
+					inputArray[j] = inputArray[j + 1];
+					inputArray[j + 1] = temp;
+				}
+			}
+		}
 	}
+	
+	public void insertSort(int[] inputArray) {
+		for (int i = 1; i < ARRAY_SIZE; i++) {
+			int temp = inputArray[i];
+			int j = i;
+			
+			while (j > 0 && inputArray[j - 1] > temp) {
+				inputArray[j] = inputArray[j - 1];
+				j--;
+			}
+			
+			inputArray[j] = temp;
+		}
+	}
+	
+//  private void swap(int[] inputArray, int i, int j) {
+//      int temp = inputArray[i];
+//      inputArray[i] = inputArray[j];
+//      inputArray[j] = temp;
+//  }
 	
 	public void print() {
 		printArray(arrayA);
@@ -37,7 +68,17 @@ public class SortingAlgorithms {
 
 	public static void main(String[] args){
 		SortingAlgorithms bubbleArray = new SortingAlgorithms();
-		
+		System.out.print("Bubble\n");
 		bubbleArray.print();
+		bubbleArray.sort("bubble");
+		bubbleArray.print();
+		
+		SortingAlgorithms insertArray = new SortingAlgorithms();
+		System.out.print("Insert\n");
+		insertArray.print();
+		insertArray.sort("insert");
+		insertArray.print();
+		
+		
 	}
 }
