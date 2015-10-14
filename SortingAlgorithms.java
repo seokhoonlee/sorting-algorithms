50e1ff1197288d06cc7864c8aaa65f68296ee3f3
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2015-10-14 20:45:23 +0800

    Add BucketSort.java

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class SortingAlgorithms {
-	public final int ARRAY_SIZE = 5;
 	public final String BUBBLE = "bubble";
 	public final String INSERT = "insert";
 	public final String SELECT = "select";
+	public final String BUCKET = "bucket";
 	public int[] arrayA = {5, 4, 3, 2, 1}; // descending order
 	public int[] arrayB = {6, 10, 7, 9, 8}; // random order
 	public int[] arrayC = {11, 12, 13, 14, 15}; // ascending order
@@ -15,6 +17,7 @@ public class SortingAlgorithms {
 			case BUBBLE: bubbleSort(arrayA); bubbleSort(arrayB); bubbleSort(arrayC); break;
 			case INSERT: insertSort(arrayA); insertSort(arrayB); insertSort(arrayC); break;
 			case SELECT: selectSort(arrayA); selectSort(arrayB); selectSort(arrayC); break;
+			case BUCKET: bucketSort(arrayA, 1, 5); bucketSort(arrayB, 6, 10); bucketSort(arrayC, 11, 15); break;
 			default: break;
 		}
 	}
@@ -22,8 +25,8 @@ public class SortingAlgorithms {
 	public void bubbleSort(int[] inputArray) {
 		int temp;
 		
-		for (int i = 0; i < ARRAY_SIZE; i++) {
-			for (int j = 0; j < ARRAY_SIZE - i - 1; j++) {
+		for (int i = 0; i < inputArray.length; i++) {
+			for (int j = 0; j < inputArray.length - i - 1; j++) {
 				if (inputArray[j] > inputArray[j + 1]) {
 					temp = inputArray[j];
 					inputArray[j] = inputArray[j + 1];
@@ -34,7 +37,7 @@ public class SortingAlgorithms {
 	}
 	
 	public void insertSort(int[] inputArray) {
-		for (int i = 1; i < ARRAY_SIZE; i++) {
+		for (int i = 1; i < inputArray.length; i++) {
 			int temp = inputArray[i];
 			int j = i;
 			
@@ -48,10 +51,10 @@ public class SortingAlgorithms {
 	}
 	
 	public void selectSort(int[] inputArray) {
-		for (int i = 0; i < ARRAY_SIZE - 1; i++) {
+		for (int i = 0; i < inputArray.length - 1; i++) {
 			int tempIndex = i;
 			
-			for (int j = i + 1; j < ARRAY_SIZE; j++) {
+			for (int j = i + 1; j < inputArray.length; j++) {
 				if (inputArray[j] < inputArray[tempIndex]) {
 					tempIndex = j;
 				}
@@ -60,6 +63,25 @@ public class SortingAlgorithms {
 			int temp = inputArray[tempIndex];
 			inputArray[tempIndex] = inputArray[i];
 			inputArray[i] = temp;
+		}
+	}
+
+	public void bucketSort(int[] inputArray, int lowerEnd, int upperEnd) {
+		int[] bucketArray = new int[upperEnd - lowerEnd + 1];
+		Arrays.fill(bucketArray, 0);
+
+		for (int i = 0; i < inputArray.length; i++) {
+			bucketArray[inputArray[i] - lowerEnd]++;
+		}
+		
+		int j = 0;
+		
+		for (int i = 0; i < upperEnd - lowerEnd + 1; i++) {
+			while (bucketArray[i] != 0) {
+				inputArray[j] = lowerEnd + i;
+				j++;
+				bucketArray[i]--;
+			}
 		}
 	}
 	
@@ -71,7 +93,7 @@ public class SortingAlgorithms {
 	}
 	
 	public void printArray(int[] inputArray) {
-		for (int i = 0; i < ARRAY_SIZE; i++) {
+		for (int i = 0; i < inputArray.length; i++) {
 			System.out.print("|");
 			System.out.print(inputArray[i]);
 		}
@@ -95,6 +117,12 @@ public class SortingAlgorithms {
 		selectArray.print();
 		selectArray.sort("select");
 		selectArray.print();
+		
+		SortingAlgorithms bucketArray = new SortingAlgorithms();
+		System.out.print("Bucket\n");
+		bucketArray.print();
+		bucketArray.sort("bucket");
+		bucketArray.print();
 
 		/*
 		 *	Output:
@@ -105,6 +133,9 @@ public class SortingAlgorithms {
 		 *	|5|4|3|2|1|6|10|7|9|8|11|12|13|14|15|
 		 *	|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|
 		 *	Select
+		 *	|5|4|3|2|1|6|10|7|9|8|11|12|13|14|15|
+		 *	|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|
+		 *	Bucket
 		 *	|5|4|3|2|1|6|10|7|9|8|11|12|13|14|15|
 		 *	|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|
 		 */
